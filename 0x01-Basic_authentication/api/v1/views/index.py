7d702586890fea78db287370e15619665424a03d
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-08-06 18:45:48 -0700

    done

---
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Module of Index views.
+"""
+from flask import jsonify, abort
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """didkgulhjlg
+    Return:
+    fhufhjlhvjlhjfhjl
+    """
+    return jsonify({"status": "OK"})
+
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """sfhtj
+    Return:
+    JSdghgdhj
+    """
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
+    """rwgarhaerhsth
+    """
+    abort(401)
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """bmcvb,hjjjjjjjjjjjjjj
+    """
+    abort(403)
